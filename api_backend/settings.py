c3b2aeb8101140e518f953b643607b617a26247d
Hassan Ahmed <ahmed.hassan.112.ha@gmail.com>
2023-12-27 16:18:35 +0500

    make deployable

--- api_backend/settings.py
+++ api_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2m^3vin7g(w%m)(==s=pm*f97p4^vsuuzkmoqdjhqeumns6%_+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (BASE_DIR / 'static',)
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
